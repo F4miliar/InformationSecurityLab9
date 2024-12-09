f887c993030affe409e7aeb2fddef5028b73f913
Maxim Lyubimov <maxim.lubimow2014@yandex.ru>
2024-12-09 23:29:24 +0400

    Добавьте файлы проекта.

---
+++ InformationSecurityLab9/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace InformationSecurityLab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDecrypted = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypted = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxDecrypted
+            // 
+            this.textBoxDecrypted.Location = new System.Drawing.Point(12, 12);
+            this.textBoxDecrypted.Multiline = true;
+            this.textBoxDecrypted.Name = "textBoxDecrypted";
+            this.textBoxDecrypted.Size = new System.Drawing.Size(291, 135);
+            this.textBoxDecrypted.TabIndex = 0;
+            // 
+            // textBoxEncrypted
+            // 
+            this.textBoxEncrypted.Location = new System.Drawing.Point(309, 12);
+            this.textBoxEncrypted.Multiline = true;
+            this.textBoxEncrypted.Name = "textBoxEncrypted";
+            this.textBoxEncrypted.Size = new System.Drawing.Size(291, 135);
+            this.textBoxEncrypted.TabIndex = 1;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(265, 153);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(82, 45);
+            this.buttonEncrypt.TabIndex = 2;
+            this.buttonEncrypt.Text = "Шифровать";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 210);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBoxEncrypted);
+            this.Controls.Add(this.textBoxDecrypted);
+            this.Name = "Form1";
+            this.Text = "Одноразовая система подстановки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDecrypted;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.Button buttonEncrypt;
+    }
+}
+
---
+++ InformationSecurityLab9/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InformationSecurityLab9
+{
+    public partial class Form1 : Form
+    {
+
+        private bool isEncrypted = false;
+        private int M = 32;
+        private int[] encryptKeys;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+
+            if (isEncrypted)
+            {
+                textBoxDecrypted.Text = Decrypt(textBoxEncrypted.Text.ToLower(), encryptKeys);
+                textBoxEncrypted.Text = String.Empty;
+                buttonEncrypt.Text = "Шифровать";
+                isEncrypted = false;
+            }
+            else
+            {
+                encryptKeys = RandomKeysGen(textBoxDecrypted.Text.Length);
+                textBoxEncrypted.Text = Encrypt(textBoxDecrypted.Text.ToLower(), encryptKeys);
+                textBoxDecrypted.Text = String.Empty;
+                buttonEncrypt.Text = "Расшифровать";
+                isEncrypted = true;
+            }
+        }
+
+        private string Encrypt(string input, int[] keys)
+        {
+            char[] array = input.ToCharArray();
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                char c = (char)((array[i] - 'а' + keys[i]) % M);
+                builder.Append((char)(c + 'а'));
+            }
+            return builder.ToString();
+        }
+
+        private string Decrypt(string input, int[] keys)
+        {
+            char[] array = input.ToCharArray();
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                char c = (char)((array[i] - 'а' - keys[i]) % M);
+                builder.Append((char)(c + 'а'));
+            }
+            return builder.ToString().ToLower();
+        }
+
+        private int[] RandomKeysGen(int size)
+        {
+            int[] keys = new int[size];
+
+            Random random = new Random();
+            for (int i = 0; i < size; i++) 
+            { 
+                keys[i] = random.Next(M);
+            }
+
+            return keys;
+        }
+    }
+}
